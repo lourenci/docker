7bdd4ceb16727bbf38b6417086d6b7e0172d235b
Leandro Lourenci <leandrolourenci@gmail.com>
2017-02-17 11:23:54 -0200

    Expose to host the 8080 port for npm run dev

--- windows-commands/npm.bat
+++ windows-commands/npm.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run --rm -it -v %cd%:/usr/src/app -w /usr/src/app jstools npm %*
+docker run --rm -it -v %cd%:/usr/src/app -w /usr/src/app --expose=8080 -p=8080:8080 jstools npm %*
