d5f33a720f23edcb52a9f658a1f11bbd73904839
Leandro Lourenci <leandro@lourenci.com>
2017-05-23 17:48:59 -0300

    Add Java 8 and FiraCode fonts to Webstorm image

---
+++ images/webstorm/install-fira-code.sh
@@ -0,0 +1,5 @@
+# In download.sh
+for type in Bold Light Medium Regular Retina; do
+    curl -o ~/.local/share/fonts/FiraCode-${type}.ttf \
+    "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
+done
